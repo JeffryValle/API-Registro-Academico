90cf5e5fbc0662f6be50597d641cbb7bc15a85cf
Jeffry <jeffryvalle99@gmail.com>
2025-07-30 23:21:22 -0600

    Cambios

--- docker/init/scripts.sql
+++ docker/init/scripts.sql
@@ -9,3 +9,14 @@ SELECT
 FROM matriculas AS m
 INNER JOIN usuarios AS u ON u.cuenta_id = m.usuario_id
 INNER JOIN cursos AS c ON c.curso_id = m.curso_id;
+
+SELECT
+	c.curso_id as CodigoCurso,
+	c.nombre AS Curso,
+	u.nombre AS Nombre,
+	u.cuenta_id AS Cuenta,
+    u.correo as Correo
+FROM matriculas AS m
+INNER JOIN usuarios AS u ON u.cuenta_id = m.usuario_id
+INNER JOIN cursos AS c ON c.curso_id = m.curso_id
+WHERE c.curso_id = 'gfgd8erdh5asdf';
