3c5ac8b8fe92af9cb08f4002753ea37327ecbaf4
FjC-14 <ferchocastro.14@gmail.com>
2025-08-09 06:46:44 -0600

    Update cursos.model.js

---
+++ models/cursos.model.js
@@ -0,0 +1,58 @@
+import e from 'cors';
+import pool from '../config/database.js';
+import { v4 as uuidv4 } from "uuid";
+
+
+
+
+export const createCurso = async (curso) => {
+    const id = uuidv4();
+
+    const query = 'INSERT INTO cursos (curso_id, nombre, cupos) VALUES (?, ?, ?) ;'
+
+    const [result] = await pool.query(query, [id, curso.nombre, curso.cupos]);
+
+    return result;
+};
+
+
+export const findByNombre = async (nombre) => {
+
+    const query = 'SELECT nombre, cupos FROM cursos WHERE nombre = ? ;';
+
+    const [rows] = await pool.query(query, [nombre]);
+
+    return rows;
+};
+
+export const getAllCursos = async () => {
+
+    const query= 'Select * FROM cursos ;'
+
+    const [rows] = await pool.query(query);
+
+    return rows;
+};
+
+
+export const getCursoByID = async ( curso_id ) => {
+
+    const query = 'SELECT nombre, cupos FROM cursos WHERE nombre = ? ;';
+
+    const [ rows ] = await pool.query( query, [ curso_id ] );
+
+    return rows[0];
+}
+
+
+export const updateCurso = async ( curso_id, curso) => {
+
+    const query = 'UPDATE cursos SET nombre = ? , cupos = ? WHERE curso_id = ? ;'; 
+
+    const [result] = await pool.query(query, [curso.nombre, curso.cupos, curso_id]); 
+
+    return result; 
+
+}
+
+
